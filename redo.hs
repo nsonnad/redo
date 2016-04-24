1b2902103b70ad70b978448abf57c39cca1e3a32
Nikhil S <nikhil.sonnad@gmail.com>
2016-04-24 16:15:18 -0400

    add redo ifchange and env var; vid6]

--- redo.hs
+++ redo.hs
@@ -1,11 +1,23 @@
+ {-# LANGUAGE StandaloneDeriving #-}
+
 import Control.Monad (filterM, liftM)
+import Data.Map.Lazy (insert, fromList, toList, adjust)
 import Data.Maybe (listToMaybe)
+import Debug.Trace (traceShow)
 import System.Directory (renameFile, doesFileExist)
-import System.Environment (getArgs)
+import System.Environment (getArgs, getEnvironment)
 import System.Exit (ExitCode(..))
 import System.FilePath (hasExtension, replaceBaseName, takeBaseName)
 import System.IO (hPutStrLn, stderr)
 import System.Process
+
+deriving instance Show CreateProcess
+
+deriving instance Show StdStream
+
+deriving instance Show CmdSpec
+
+traceShow' arg = traceShow arg arg
 
 main :: IO ()
 main = mapM_ redo =<< getArgs
@@ -14,14 +26,16 @@ redo :: String -> IO ()
 redo target = maybe printMissing redo' =<< redoPath target
   where redo' :: FilePath -> IO ()
         redo' path = do
-          (_, _, _, ph) <- createProcess $ shell $ cmd path
+          oldEnv <- getEnvironment
+          let newEnv = toList $ adjust (++ ":.") "PATH" $ insert "REDO_TARGET" target $ fromList oldEnv
+          (_, _, _, ph) <- createProcess $ traceShow' $ (shell $ cmd path) {env = Just newEnv}
           exit <- waitForProcess ph
-          case exit of
+          case traceShow' exit of
             ExitSuccess -> do renameFile tmp target
             ExitFailure code -> do hPutStrLn stderr $ "Redo script exited with non-zero exit code: " ++ show code
         tmp = target ++ "---redoing"
         printMissing = error $ "No .do file found for target " ++ target
-        cmd path = unwords ["sh", path, "0", takeBaseName target, tmp]
+        cmd path = traceShow' $ unwords ["sh", path, "0", takeBaseName target, tmp]
 
 redoPath :: FilePath -> IO (Maybe FilePath)
 redoPath target = listToMaybe `liftM` filterM doesFileExist candidates
